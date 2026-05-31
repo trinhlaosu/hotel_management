68429122fbda1c49512caedb69e8547d44b5b6f8
Lê Đức Minh <ducminh@MacBook-Air-cua-Le.local>
2026-05-31 15:12:27 +0700

    change postman cllection

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':   'hotel_management',
         'USER':   'root',
-        'PASSWORD': '123456',        
+        'PASSWORD': '',        
         'HOST':   '127.0.0.1',
         'PORT':   '3306',
         'OPTIONS': {'charset': 'utf8mb4'},
